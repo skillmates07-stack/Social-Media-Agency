66f022599c0ac8acc029fb27d1622b15e3df19d0
skillmates07-stack <skillmates07@gmail.com>
2025-10-30 00:53:25 +0530

    Update next.config.js

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -1,12 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-  typescript: {
-    ignoreBuildErrors: true,
-  },
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
-}
+const nextConfig = {}
 
 module.exports = nextConfig
