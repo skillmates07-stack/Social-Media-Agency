32706604cbd8dd08ce6bb91d9516b216359469dc
skillmates07-stack <skillmates07@gmail.com>
2025-10-30 22:21:13 +0530

    Delete apps/api/src/types directory

--- apps/api/src/types/fastify.d.ts
+++
@@ -1,8 +0,0 @@
-import 'fastify';
-import { FastifyRequest, FastifyReply } from 'fastify';
-
-declare module 'fastify' {
-  interface FastifyInstance {
-    authenticate: (request: FastifyRequest, reply: FastifyReply) => Promise<void>;
-  }
-}
